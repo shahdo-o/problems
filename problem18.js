31f9e5f09de96e37ad4debeae463c4b9ae7c2445
shahdo-o <142066665+shahdo-o@users.noreply.github.com>
2023-08-25 19:14:11 +0300

    Update problem18.js

--- problem18.js
+++ problem18.js
@@ -1,11 +1,7 @@
-let l1="ma5louf.org";//tm
-let l2="https://ma5louf.org";//tm
-let l3="http://ma5louf.com";//tm
-let l4="https://www.ma5louf.org";//tm
-let l5="https://www.ma5louf.org:8080/art.php?id=1111cat=top";
-let l= /(https?:\/\/)?(www.)?ma5louf.(com|org)(:8080\/art.php\?id=1111cat=top)?/ig;
-console.log(l1.match(l));
-console.log(l2.match(l));
-console.log(l3.match(l));
-console.log(l4.match(l));
-console.log(l5.match(l));
+t=prompt("enter your Email");
+function reg(t){
+ let l= /(https?:\/\/)?(www.)?ma5louf.(com|org)(:8080\/art.php\?id=1111cat=top)?/ig;
+ return l.test(t);
+};
+
+console.log(reg(t));
