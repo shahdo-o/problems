e898a3b7a95b5b3ecfd5fa046270abd74223370d
shahdo-o <142066665+shahdo-o@users.noreply.github.com>
2023-08-25 18:21:42 +0300

    Update problem15.js

--- problem15.js
+++ problem15.js
@@ -1,16 +1,14 @@
 l=5;//5*4*3*2
 function rec(l)
-{
-    
-  if(l===1){
+{   
+  if(l===1)
+  {
     return 1;
   }
-  else{
+  else
+  {
     return (l*rec(l-1));
   }
 
 };
-
-
-
 console.log(rec(l));
