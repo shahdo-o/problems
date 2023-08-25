824f5ea8d009d39be9993e7a2de4e88a417c332d
shahdo-o <142066665+shahdo-o@users.noreply.github.com>
2023-08-25 19:18:09 +0300

    Update problem18.js

--- problem18.js
+++ problem18.js
@@ -1,7 +1,7 @@
 t=prompt("enter your Email");
 function reg(t){
  let l= /(https?:\/\/)?(www.)?ma5louf.(com|org)(:8080\/art.php\?id=1111cat=top)?/ig;
- return l.test(t);
+ return l.test(t) ?  "It's a correct email " : "It's a wrong email.";
 };
 
 console.log(reg(t));
